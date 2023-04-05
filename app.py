11560aeec6b398c572b501b40dd52832369838e5
Nathan <napa8745@colorado.edu>
2023-04-05 03:01:02 +0000

    changes pushed

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://lab10db_user:t43K9ALBZmgBQRax2uqi6xoypoR3awHc@dpg-cgme5qjk9u59cru7vd60-a/lab10db")
+    conn.close()
+    return "Database Connection Successful"
+    
+
